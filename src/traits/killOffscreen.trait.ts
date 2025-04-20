c9f96368d3469bceab095d3ea90cc7d00c95890f
kktos <tm.barral@gmail.com>
2025-04-20 08:57:06 +0200

    fix: use name to get Trait

--- src/traits/killOffscreen.trait.ts
+++ src/traits/killOffscreen.trait.ts
@@ -2,12 +2,12 @@ import type { Entity } from "../entities/Entity";
 import type GameContext from "../game/GameContext";
 import type { Scene } from "../scene/Scene";
 import { Trait } from "./Trait";
-import { KillableTrait } from "./killable.trait";
+import type { KillableTrait } from "./killable.trait";
 
 export class KillIfOffscreenTrait extends Trait {
 	update(gc: GameContext, entity: Entity, scene: Scene) {
 		if (scene.bbox.left <= entity.left && entity.left <= scene.bbox.right && entity.top <= scene.bbox.bottom) return;
-		const trait = entity.traits.get(KillableTrait) as KillableTrait;
+		const trait = entity.traits.get("KillableTrait") as KillableTrait;
 		if (trait) trait.kill();
 	}
 }
