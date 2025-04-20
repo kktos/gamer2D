9147dfe436b1de57a78195d1aefcea2f2aad15bd
kktos <tm.barral@gmail.com>
2025-04-20 08:56:22 +0200

    fix: reset VarDict before compiling

--- src/script/compiler/compiler.ts
+++ src/script/compiler/compiler.ts
@@ -6,6 +6,7 @@ const parser = new SheetParser();
 export function compileScript(text: string) {
 	const lexingResult = SheetLexer.tokenize(text);
 	parser.input = lexingResult.tokens;
+	parser.variablesDict = new Map();
 	const result = parser.sheet();
 	if (parser.errors.length > 0) {
 		console.error("PARSE ERR", parser.errors);
