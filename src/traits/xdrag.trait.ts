8a2aa0adc91d4fa44ce4a321ec97e0bc18d59ea5
kktos <tm.barral@gmail.com>
2025-04-28 14:44:57 +0200

    fix: xdrag trait

--- src/traits/xdrag.trait.ts
+++ src/traits/xdrag.trait.ts
@@ -3,14 +3,14 @@ import type { Scene } from "../scene/Scene";
 import { Trait } from "./Trait";
 
 export class XDragTrait extends Trait {
-	constructor(private deceleration = 400) {
+	constructor(public dragFactor = 400) {
 		super();
 	}
 
 	update({ dt }, entity: Entity, _: Scene) {
 		if (entity.vel.x === 0) return;
 		const absX = Math.abs(entity.vel.x);
-		const decel = Math.min(absX, this.deceleration * dt);
+		const decel = Math.min(absX, this.dragFactor * dt);
 		entity.vel.x += entity.vel.x > 0 ? -decel : decel;
 	}
 }
