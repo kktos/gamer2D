99f5ab6f1a299abe0cd6582af11fb74bc697be71
kktos <tm.barral@gmail.com>
2025-04-30 20:06:35 +0200

    feat: update postbuild to inc version

--- build/postbuild.mjs
+++ build/postbuild.mjs
@@ -5,6 +5,12 @@ const keys= [ "name", "version", "type", "author", "description", "license", "mo
 const packageJsonRaw = readFileSync("./package.json", "utf-8");
 const packageJson = JSON.parse(packageJsonRaw);
 
+const parts= packageJson.version.split(".");
+parts[1]= parseInt(parts[1])+1;
+packageJson.version= parts.join(".");
+console.log("VERSION",packageJson.version);
+writeFileSync("./package.json", JSON.stringify(packageJson, null, 4));
+
 const newPackageJson = Object.keys(packageJson)
 					.filter(key => keys.includes(key))
 					.reduce((acc, cur) => {
@@ -13,3 +19,4 @@ const newPackageJson = Object.keys(packageJson)
 
 writeFileSync("./dist/package.json", JSON.stringify(newPackageJson, null, 4));
 copyFileSync("./README.md","./dist/README.md");
+
