6663ba2e789c3b898170983aa7256cbfd4704374
kktos <tm.barral@gmail.com>
2025-05-04 16:35:34 +0200

    feat: add traits on Entity in Entities layer

--- src/utils/createLevelEntities.utils.ts
+++ src/utils/createLevelEntities.utils.ts
@@ -1,17 +1,23 @@
 import type { Entity } from "../entities/Entity";
 import { createEntityByName } from "../entities/Entity.factory";
 import type ResourceManager from "../game/ResourceManager";
+import { addTraits } from "../layers/display/trait.manager";
 import type { Grid } from "../maths/grid.math";
+import { GLOBAL_VARIABLES } from "../scene/Scene.factory";
 import type { TEntitiesLayerSprite } from "../script/compiler/layers/entities/entities.rules";
+import { TVars } from "./vars.utils";
 
 export function createLevelEntities(resourceManager: ResourceManager, grid: Grid, sprites: TEntitiesLayerSprite[]) {
 	const entities: Entity[] = [];
-	if (sprites)
+	if (sprites) {
+		const vars = new TVars(GLOBAL_VARIABLES, GLOBAL_VARIABLES);
 		for (const sprite of sprites) {
 			const [x, y] = grid.toCoord(sprite.pos[0] as number, sprite.pos[1] as number);
 			const entity = createEntityByName(resourceManager, sprite.name, x, y, sprite.dir);
 			if (sprite.id) entity.id = sprite.id;
+			if (sprite.traits) addTraits(sprite.traits, entity, vars);
 			entities.push(entity);
 		}
+	}
 	return entities;
 }
