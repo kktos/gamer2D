7ee8a3a8da2468109e811399df4e2660995dd781
kktos <tm.barral@gmail.com>
2025-05-04 16:38:47 +0200

    refacto: error handling on compile

--- src/script/compiler/compiler.ts
+++ src/script/compiler/compiler.ts
@@ -3,14 +3,21 @@ import { SheetParser } from "./parser";
 
 const parser = new SheetParser();
 
-function compile(text: string, startRule: string, globals?: Map<string, unknown>) {
+let wannaLogError = false;
+export function setWannaLogError(value: boolean) {
+	wannaLogError = value;
+}
+
+export function compile(text: string, startRule: string, globals?: Map<string, unknown>) {
 	const lexingResult = SheetLexer.tokenize(text);
 	parser.input = lexingResult.tokens;
 	parser.variablesDict = globals ? globals : new Map();
 	const result = parser[startRule]();
 	if (parser.errors.length > 0) {
-		console.error("PARSE ERR", parser.errors);
-		throw new Error("PARSE ERR");
+		if (wannaLogError) console.error(parser.errors);
+		const line = parser.errors[0].token.startLine;
+		const word = parser.errors[0].token.image;
+		throw new SyntaxError(`SYNTAX ERROR LINE ${line} at "${word}"`, { cause: parser.errors });
 	}
 	return result;
 }
