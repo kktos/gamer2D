a1ec71c31fc884633d0761a384f51570cb798754
kktos <tm.barral@gmail.com>
2025-05-04 16:41:06 +0200

    feat: layer globals

---
+++ src/script/compiler/layers/globals/globals.rules.ts
@@ -0,0 +1,31 @@
+import { tokens } from "../../lexer";
+import type { TSet } from "../display/layout/set.rules";
+
+export type TGlobalsLayerSheet = {
+	type: "globals";
+	variables: TSet[];
+};
+
+// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
+export class GlobalsLayerRules {
+	static globalsLayerSheet($) {
+		return $.RULE("globalsLayerSheet", () => {
+			const sheet: TGlobalsLayerSheet = { type: "globals", variables: [] };
+
+			$.CONSUME(tokens.Globals);
+			$.CONSUME(tokens.OpenCurly);
+
+			$.MANY(() => {
+				sheet.variables.push($.SUBRULE($.layoutSet));
+			});
+
+			$.ACTION(() => {
+				if (sheet.variables.length === 0) throw new TypeError("No variables defined in globals layer");
+			});
+
+			$.CONSUME(tokens.CloseCurly);
+
+			return sheet;
+		});
+	}
+}
---
+++ src/script/compiler/layers/globals/globals.test.ts
@@ -0,0 +1,30 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { OP_TYPES } from "../../../../types/operation.types";
+import { compileLayerScript } from "../../compiler";
+
+describe("User Defined Layers", () => {
+	let globals: Map<string, unknown>;
+
+	beforeEach(() => {
+		globals = new Map();
+	});
+
+	it("should raises an error when the layer is empty", () => {
+		const script = `
+		layer globals {
+		}
+		`;
+		expect(() => compileLayerScript(script)).toThrow(/No variables defined in globals layer/);
+	});
+
+	it("should deal with empty user layer", () => {
+		const script = `
+		layer globals {
+			$p=0
+		}
+		`;
+		const result = compileLayerScript(script);
+		expect(result).toBeDefined();
+		expect(result).toEqual({ type: "globals", variables: [{ type: OP_TYPES.SET, name: "p", value: 0 }] });
+	});
+});
