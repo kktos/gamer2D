aa644690def5dbc15e59fb6330465f54c81c8ecb
kktos <tm.barral@gmail.com>
2025-05-04 16:34:50 +0200

    refacto: xdragtrait with min vel

--- src/traits/xdrag.trait.ts
+++ src/traits/xdrag.trait.ts
@@ -3,7 +3,10 @@ import type { Scene } from "../scene/Scene";
 import { Trait } from "./Trait";
 
 export class XDragTrait extends Trait {
-	constructor(public dragFactor = 400) {
+	constructor(
+		public dragFactor = 400,
+		public minVelocity = 0,
+	) {
 		super();
 	}
 
@@ -11,6 +14,7 @@ export class XDragTrait extends Trait {
 		if (entity.vel.x === 0) return;
 		const absX = Math.abs(entity.vel.x);
 		const decel = Math.min(absX, this.dragFactor * dt);
-		entity.vel.x += entity.vel.x > 0 ? -decel : decel;
+		const newVelocity = Math.max(absX - decel, this.minVelocity);
+		entity.vel.x = entity.vel.x > 0 ? newVelocity : -newVelocity;
 	}
 }
