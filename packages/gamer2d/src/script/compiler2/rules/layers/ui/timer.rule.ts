b8f168136cf599d3fd45d7601ee92d7074219116
kktos <tm.barral@gmail.com>
2025-07-04 15:00:55 +0200

    feat: parse timer

--- packages/gamer2d/src/script/compiler2/rules/layers/ui/timer.rule.ts
+++ packages/gamer2d/src/script/compiler2/rules/layers/ui/timer.rule.ts
@@ -1,3 +1,4 @@
+import { generateID } from "../../../../../utils/id.util";
 import type { NeatParser } from "../../../parser";
 import type { TNeatTimerCommand } from "../../../types/commands.type";
 import { parseValueExpression } from "../../shared/value-expr.rule";
@@ -5,7 +6,10 @@ import { parseValueExpression } from "../../shared/value-expr.rule";
 export function parseTimer(parser: NeatParser) {
 	parser.consume("IDENTIFIER", "timer");
 
-	const result: TNeatTimerCommand = { cmd: "TIMER", id: parser.name(), isRepeating: false, duration: [{ type: "const", value: 0 }] };
+	const result: TNeatTimerCommand = { cmd: "TIMER", id: "", isRepeating: false, duration: [{ type: "const", value: 0 }] };
+
+	if (parser.isIdentifier(["every", "after"])) result.id = generateID();
+	else result.id = parser.name();
 
 	const tok = parser.identifier(["every", "after"]);
 	result.isRepeating = tok === "every";
