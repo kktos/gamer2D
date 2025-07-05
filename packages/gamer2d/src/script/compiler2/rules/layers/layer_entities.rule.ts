c91cb9872090544bb01698c8f8e9b3c93889ed9d
kktos <tm.barral@gmail.com>
2025-07-05 21:27:52 +0200

    feat: add timer cmd to layer entities

--- packages/gamer2d/src/script/compiler2/rules/layers/layer_entities.rule.ts
+++ packages/gamer2d/src/script/compiler2/rules/layers/layer_entities.rule.ts
@@ -1,28 +1,37 @@
 import type { NeatParser } from "../../parser";
-import { TNeatCommand } from "../../types/commands.type";
+import type { TNeatCommand } from "../../types/commands.type";
+import { parseVariableAssignment } from "../shared/assign.rule";
 import { parseFor } from "../shared/for.rule";
+import { parseOn } from "../shared/on.rule";
 import { parseSettings } from "../shared/settings.rule";
 import { parseSprite } from "../shared/sprite.rule";
+import { parsePool } from "./ui/pool.rule";
+import { parseTimer } from "./ui/timer.rule";
+
+const COMMAND_HANDLERS = {
+	on: parseOn,
+	sprite: parseSprite,
+	pool: parsePool,
+	for: parseFor,
+	timer: parseTimer,
+	settings: parseSettings,
+} as const;
 
 export function parseLayerEntites(parser: NeatParser) {
 	parser.punct("{");
 
 	const result: TNeatCommand[] = [];
 
-	loop: while (parser.is("IDENTIFIER")) {
-		switch (parser.peekValue()) {
-			case "settings":
-				result.push(parseSettings(parser));
-				break;
-			case "for":
-				result.push(parseFor(parser));
-				break;
-			case "sprite":
-				result.push(parseSprite(parser));
-				break;
-			default:
-				break loop;
+	while (parser.is(["IDENTIFIER", "VARIABLE"])) {
+		if (parser.is("VARIABLE")) {
+			result.push(parseVariableAssignment(parser));
+			continue;
 		}
+
+		const commandName = parser.peekValue();
+		const handler = COMMAND_HANDLERS[commandName as keyof typeof COMMAND_HANDLERS];
+		if (!handler) break;
+		result.push(handler(parser));
 	}
 
 	parser.punct("}");
