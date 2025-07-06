9549b3bbe79e33e9534bb488a1563f6a42dd24ce
kktos <tm.barral@gmail.com>
2025-07-06 14:21:03 +0200

    refacto: globals layer - comment out dead code

--- packages/gamer2d/src/layers/globals.layer.ts
+++ packages/gamer2d/src/layers/globals.layer.ts
@@ -1,25 +1,24 @@
 import type { GameContext } from "../game/types/GameContext";
 import type { Scene } from "../scenes/Scene";
-import type { TSet } from "../script/compiler/layers/display/layout/set.rules";
 import { runCommands } from "../script/engine2/exec";
 import type { ExecutionContext } from "../script/engine2/exec.type";
-import type { NeatFunctions } from "../script/engine2/functions/functionDict.utils";
+import type { NeatFunctions } from "../script/engine2/functions/functions.store";
 import { createVariableStore } from "../utils/vars.store";
 import { Layer } from "./Layer.class";
 
-class VarSetDict extends Map<string, TSet> {
-	[Symbol.for("inspect")]() {
-		const result = document.createElement("ul");
-		let html = "";
-		for (const [name, varValue] of this.entries()) {
-			const value = varValue.value;
-			const displayValue = value[Symbol.for("inspect")] ? value[Symbol.for("inspect")]() : value;
-			html += `<li>${name}: ${displayValue}</li>`;
-		}
-		result.innerHTML = html;
-		return result;
-	}
-}
+// class VarSetDict extends Map<string, TSet> {
+// 	[Symbol.for("inspect")]() {
+// 		const result = document.createElement("ul");
+// 		let html = "";
+// 		for (const [name, varValue] of this.entries()) {
+// 			const value = varValue.value;
+// 			const displayValue = value[Symbol.for("inspect")] ? value[Symbol.for("inspect")]() : value;
+// 			html += `<li>${name}: ${displayValue}</li>`;
+// 		}
+// 		result.innerHTML = html;
+// 		return result;
+// 	}
+// }
 
 export class GlobalsLayer extends Layer {
 	constructor(gc: GameContext, parent: Scene, sheet) {
